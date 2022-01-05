673aecaacd46f5b85f54538e73f4bf8067f3010d
Juan Pablo Botero <boterobpablo@utp.edu.co>
2022-01-05 07:54:09 -0500

    commitComun

---
+++ src/app/consultas/models/medico.ts
@@ -0,0 +1,9 @@
+export class Medico {
+    id: number;
+    nombre: string;
+    apellido: string;
+    documentoIdentidad: number;
+    profesion: string;
+    telefono: number;
+    email: string;
+}
